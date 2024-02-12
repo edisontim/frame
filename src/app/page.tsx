2f1b22aceedf9f601b66f2e187d1f206f84dae09
Timothy Edison <timothyedison@hotmail.com>
2024-02-12 19:04:34 +0100

    valid image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,10 @@ export default function Home() {
   return (
     <>
       <meta property="fc:frame" content="vNext" />
-      <meta property="fc:frame:image" content="http://...image-question.png" />
+      <meta
+        property="fc:frame:image"
+        content="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyLTD-EEs8bxwuZtNSWPEROeaAu3FvGnlGQ6BhxGc4fuuphh9jJz1OWtJQII7BMUlrCHA&usqp=CAU"
+      />
       <meta property="fc:frame:button:1" content="Green" />
       <meta property="fc:frame:button:2" content="Purple" />
       <meta property="fc:frame:button:3" content="Red" />
