f744b35a415f365fe02f370286b642c0d9bb025a
Timothy Edison <timothyedison@hotmail.com>
2024-02-14 14:08:12 +0100

    Works locally

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,11 @@ import {
   FrameImage,
   FrameReducer,
   NextServerPageProps,
+  getFrameMessage,
   getPreviousFrame,
   useFramesReducer,
-  getFrameMessage,
 } from "frames.js/next/server";
+import Link from "next/link";
 
 type State = {
   score: number;
@@ -30,10 +31,15 @@ export default async function Home({
   searchParams,
 }: NextServerPageProps) {
   const previousFrame = getPreviousFrame<State>(searchParams);
-  console.log(`Hereeee !`, previousFrame);
-  const frameMessage = await getFrameMessage(previousFrame.postBody, {});
 
-  if (frameMessage) {
+  const frameMessage = await getFrameMessage(previousFrame.postBody, {
+    hubHttpUrl:
+      process.env.NODE_ENV === "development"
+        ? "http://localhost:3000/debug/hub"
+        : undefined,
+  });
+
+  if (frameMessage && !frameMessage?.isValid) {
     throw new Error("Invalid frame payload");
   }
 
@@ -45,37 +51,34 @@ export default async function Home({
 
   // Here: do a server side side effect either sync or async (using await), such as minting an NFT if you want.
   // example: load the users credentials & check they have an NFT
-
   console.log("info: state is:", state);
 
-  if (frameMessage) {
-    const { buttonIndex } = frameMessage;
-
-    console.log("info: buttonIndex is:", buttonIndex);
-  }
-  // then, when done, return next frame
   return (
-    <FrameContainer
-      postUrl="/frames"
-      state={state}
-      previousFrame={previousFrame}
-    >
-      <FrameImage>
-        <div
-          style={{ display: "flex", flexDirection: "column" }}
-          tw="w-full h-full bg-slate-700 text-white justify-center items-center"
-        >
-          <p>lets keep this at 420: </p>
-          <br />
-          <p>{state.score}</p>
-        </div>
-      </FrameImage>
-      <FrameButton>{`🔽${
-        state.score >= 420 ? (state.score == 420 ? "" : "😇") : "😈"
-      }`}</FrameButton>
-      <FrameButton>{`🔼${
-        state.score >= 420 ? (state.score == 420 ? "" : "😈") : "😇"
-      }`}</FrameButton>
-    </FrameContainer>
+    <div>
+      Hello data
+      <FrameContainer
+        postUrl="/frames"
+        state={state}
+        previousFrame={previousFrame}
+        pathname="/"
+      >
+        <FrameImage>
+          <div
+            style={{ display: "flex", flexDirection: "column" }}
+            tw="w-full h-full bg-slate-700 text-white justify-center items-center"
+          >
+            <p>lets keep this at 420: </p>
+            <br />
+            <p>{state.score}</p>
+          </div>
+        </FrameImage>
+        <FrameButton>{`🔽${
+          state.score >= 420 ? (state.score == 420 ? "" : "😇") : "😈"
+        }`}</FrameButton>
+        <FrameButton>{`🔼${
+          state.score >= 420 ? (state.score == 420 ? "" : "😈") : "😇"
+        }`}</FrameButton>
+      </FrameContainer>
+    </div>
   );
 }
