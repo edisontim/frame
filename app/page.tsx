e7f256427a9f4363a33fded178e31c6e6422f5d6
Timothy Edison <timothyedison@hotmail.com>
2024-02-15 16:18:49 +0100

    Use urls and input text for speed

--- app/page.tsx
+++ app/page.tsx
@@ -6,9 +6,14 @@ import {
   getPreviousFrame,
   useFramesReducer,
   FrameImage,
+  FrameInput,
 } from "frames.js/next/server";
 
 import { FrameContainer } from "./components/FrameContainer";
+
+import { OpenAI } from "openai";
+
+const openAi = new OpenAI();
 
 const HAPPY_IMG = "happy.jpeg";
 const SAD_IMG = "sad.jpeg";
@@ -53,65 +58,43 @@ export default async function Home({
 
   const [state] = useFramesReducer<State>(reducer, initialState, previousFrame);
 
-  let gptMsg = "";
+  let gptMsg = "Perfection";
 
   let upEmoji = "";
   let downEmoji = "";
   let img = HAPPY_IMG;
   let url = HAPPY_URL;
-  let content = [
-    <FrameImage key={0} src={url} />,
-    <FrameButton key={1}>lets keep this at 420!!!</FrameButton>,
-  ];
-  if (state.score > 420) {
+
+  if (currentScore > 420) {
     upEmoji = "😈";
     downEmoji = "😇";
     img = SAD_IMG;
     url = SAD_URL;
-    gptMsg = "Oh no, please bring me back down to the ideal number.";
-  } else if (state.score < 420) {
+    gptMsg = "Oh no, bring me back down to 42";
+  } else if (currentScore < 420) {
     upEmoji = "😇";
     downEmoji = "😈";
     img = SAD_IMG;
     url = SAD_URL;
-    gptMsg = "Oh no, please bring me back up to the ideal number.";
+    gptMsg = "Oh no, bring me back up to 42";
   }
 
-  if (previousFrame.prevState != null) {
-    content = [
-      <FrameImage key={0}>
-        <div
-          style={{
-            backgroundImage: `url(${url})`,
-            backgroundSize: "100% 100%",
-            backgroundRepeat: "no-repeat",
-          }}
-          tw="w-full h-full text-black flex flex-col justify-center items-center"
-        >
-          <p style={{ background: "white" }}>lets keep this at 420: </p>
-          <p
-            style={{
-              fontSize: "1.5em",
-              fontWeight: "bold",
-              background: "white",
-            }}
-          >
-            {currentScore}
-          </p>
-          <p
-            style={{
-              maxWidth: "60vw",
-              textAlign: "center",
-              background: "white",
-            }}
-          >
-            {gptMsg}
-          </p>
-        </div>
-      </FrameImage>,
-      <FrameButton key={1}>{`🔽${downEmoji}`}</FrameButton>,
-      <FrameButton key={2}>{`🔼${upEmoji}`}</FrameButton>,
-    ];
+  if (previousFrame.prevState) {
+    const gptCompletion = await await openAi.chat.completions.create({
+      model: "gpt-4-turbo-preview",
+      messages: [
+        {
+          role: "system",
+          content: `Imagine that you're a number and your wellbeing depends on whether or not that you are at 420. I control a button that makes you go higher or lower. All you can do is output a dramatic sentence (under no circumstance can you output more than 27 characters) that expresses your wellbeing and state of mind. The user will give you the score you were before my press and the score you are after my press.`,
+        },
+        {
+          role: "user",
+          content: `The previous number was ${previousFrame.prevState?.score} and your current number is ${currentScore}`,
+        },
+      ],
+    });
+    gptMsg = gptCompletion.choices[0]?.message.content ?? String(currentScore);
+    gptMsg = gptMsg.slice(0, 27);
   }
 
   return (
@@ -123,7 +106,10 @@ export default async function Home({
         previousFrame={previousFrame}
         pathname="/"
       >
-        {...content}
+        <FrameImage src={url} aspectRatio="1.91:1"></FrameImage>
+        <FrameInput text={`${currentScore}: ${gptMsg}`}></FrameInput>
+        <FrameButton>{`🔽${downEmoji}`}</FrameButton>
+        <FrameButton>{`🔼${upEmoji}`}</FrameButton>
       </FrameContainer>
     </div>
   );
